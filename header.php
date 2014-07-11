8846963488b07b6a28cf25fc9e5d6dc3e2f4bf9c
echenley <evan.henley@gmail.com>
2014-07-11 03:26:27 -0500

    sidebar removed hr

--- header.php
+++ header.php
@@ -15,7 +15,6 @@
                 <a href="<?php echo esc_url(home_url('/')); ?>" class="title-link"><?php bloginfo('name'); ?></a>
             </h1>
             <div class="site-tagline"><?php bloginfo('description'); ?></div>
-            <hr>
             <?php wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'primary-nav', 'menu_class' => 'primary-menu')); ?>
         </header>
 
