c42fdff317e28d73f81ea4dd37336a2f6ab5625f
echenley <evan.henley@gmail.com>
2014-09-24 20:21:28 -0500

    replaced js with css animations

---
+++ attachment.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<?php get_template_part('partials/sidebar'); ?>
+<?php get_template_part('partials/content-top'); ?>
+<?php get_template_part('partials/spinner'); ?>
+<?php get_template_part('partials/content-header'); ?>
+
+<div class="post">
+	<h1 class="post-title"><?php the_title(); ?></h1>
+
+    <div class="post-body">
+       	<?php while(have_posts()): the_post(); ?>
+			<?php the_content(); ?>
+		<?php endwhile; ?>
+	</div>
+</div>
+
+<?php get_template_part('partials/content-bottom'); ?>
+<?php get_footer(); ?>
