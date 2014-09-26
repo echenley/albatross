4bf51646b994834377cd58e6fdb62f5a28d33eab
echenley <evan.henley@gmail.com>
2014-09-25 20:17:36 -0500

    removed categories/topics, added dotted underline to post-body links

--- partials/post-meta.php
+++ partials/post-meta.php
@@ -3,8 +3,10 @@
 		<img src="<?php echo get_template_directory_uri(); ?>/library/images/open-iconic/svg/clock.svg" alt="clock" class="icon">
 		<?php the_time('F jS, Y'); ?>
 	</div>
+<!-- 
 	<div class="categories post-info">
 		<img src="<?php echo get_template_directory_uri(); ?>/library/images/open-iconic/svg/folder.svg" alt="folder" class="icon">
 		<?php albatross_categories(); ?>
 	</div>
+-->
 </div>
